436834409f05c10d88fc001ac98b8bb19afbb516
olivierjuan <olivier.juan@gmail.com>
2026-02-26 23:33:12 +0000

    Deploying to gh-pages from @ olivierjuan/mal-folio.github.io@c937cc5ff7b2a3586dd8323c5ba87031f356e661 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,21 +18,21 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/mal-folio.github.io/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/mal-folio.github.io/repositories/";
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/mal-folio.github.io/cv/";

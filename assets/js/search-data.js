30737e93f5cc68bde2c3674b8b3a85d28b583e8b
olivierjuan <olivier.juan@gmail.com>
2026-02-27 22:32:47 +0000

    Deploying to gh-pages from @ olivierjuan/mal-folio.github.io@ab0f417402f3274096d7bfd21f524b4ac069da9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,46 +94,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/mal-folio.github.io/news/announcement_1/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/mal-folio.github.io/projects/9_project/";
             },},{id: "projects-bbmdp-a-markov-decision-process-for-variable-selection-in-branch-amp-bound",
           title: 'BBMDP: A Markov Decision Process for Variable Selection in Branch &amp;amp; Bound',
           description: "A principled vanilla MDP formulation for learning optimal branching strategies in MILP solvers, unlocking k-step RL algorithms previously incompatible with the TreeMDP framework. New state-of-the-art among RL agents on the Ecole benchmark. NeurIPS 2025.",

e60f4bdfc39ab8d287bc2e32cf1b6d0886cb1e88
olivierjuan <olivier.juan@gmail.com>
2026-02-27 22:37:46 +0000

    Deploying to gh-pages from @ olivierjuan/mal-folio.github.io@b736c194b13982518025c903f082c3be1bc1bb5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,12 +59,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/mal-folio.github.io/books/the_godfather/";
-            },},{id: "news-fmsts-accepted-at-cpaior-2020",
+      },{id: "news-fmsts-accepted-at-cpaior-2020",
           title: 'FMSTS accepted at CPAIOR 2020',
           description: "",
           section: "News",handler: () => {
